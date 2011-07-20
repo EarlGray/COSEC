7e8543fd3c24026533b5daa8588260237c3a9635
Dmytro Sirenko <dmytro.sirenko@ti.com>
2011-07-20 20:51:23 +0300

    merge intrs.h from net

--- include/dev/intrs.h
+++ include/dev/intrs.h
@@ -8,6 +8,9 @@
 
 typedef void (*intr_handler_f) (void *);
 
+uint16_t irq_get_mask(void);
+void irq_mask(bool set, uint8_t irq_num);
+
 void intrs_setup(void);
 
 void irq_set_handler(uint32_t irq_num, intr_handler_f handler);
