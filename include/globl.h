ed80f5e5787f8fc319f501590db6a3fab809fffb
EarlGray <dmytrish@gmail.com>
2012-09-25 14:25:07 +0300

    Configuration moved to include/conf.h from globl.h

--- include/globl.h
+++ include/globl.h
@@ -1,25 +1,5 @@
 /**********************************************************/
-#ifndef __CONF_H__
-#define __CONF_H__
-
-#define PAGE_SIZE       0x1000
-
-#define PAGING          (1)
-
-#define INTR_PROFILING  (0)
-#define MEM_DEBUG       (1)
-#define TASK_DEBUG      (0)
-#define INTR_DEBUG      (1)
-
-#if PAGING
-# define KERN_OFF       0xc0000000
-#else
-# define KERN_OFF       0x00000000
-#endif
-
-#define KERN_PA         0x00100000
-
-#endif // __CONF_H__
+#include <conf.h>
 
 #ifndef NOT_CC
 
