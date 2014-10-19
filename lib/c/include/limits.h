d9560f20c85451f19ad9890e49fe2165d80c10f0
Dmytro S <dmytrish@gmail.com>
2014-10-19 12:30:01 +0300

    add stdint.h, limits.h back

---
+++ lib/c/include/limits.h
@@ -0,0 +1,11 @@
+#ifndef __COSEC_LIMITS_H__
+#define __COSEC_LIMITS_H__
+
+#define MAX_INT     (0x7FFFFFFF)
+#define MAX_UINT    (0xFFFFFFFF)
+#define MAX_ULONG   ((MAX_UINT << 32) | MAX_UINT)
+#define UCHAR_MAX   0xFF
+
+#define INTPTR_MAX  MAX_UINT
+
+#endif //__COSEC_LIMITS_H__
---
+++ lib/c/include/stdint.h
@@ -0,0 +1,19 @@
+#ifndef __COSEC_STDINT_H__
+#define __COSEC_STDINT_H__
+
+#include <sys/types.h>
+
+typedef unsigned char	    uint8_t, byte;
+typedef unsigned short	    uint16_t;
+typedef unsigned int	    uint32_t, uint20_t, uint;
+typedef unsigned long long  uint64_t, ulong;
+
+typedef short               int16_t;
+typedef long                int32_t;
+typedef long long           int64_t;
+
+typedef uint32_t	size_t, ssize_t, ptr_t, index_t, count_t, mode_t;
+typedef int32_t     err_t;
+typedef index_t     kdev_t;
+
+#endif //__COSEC_STDINT_H__
