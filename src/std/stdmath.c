ef9e2e65a41bdfea55dda100511c4aa94b1d7817
Dmytro S <dmytrish@gmail.com>
2015-02-13 00:28:32 +0200

    std/stdmath.c

---
+++ src/std/stdmath.c
@@ -0,0 +1,102 @@
+#include <math.h>
+#include <log.h>
+
+double floor(double x) {
+    logmsge("TODO: floor()");
+    return x;
+}
+double ceil(double x) {
+    logmsge("TODO: ceil()");
+    return x;
+}
+
+double fabs(double x) {
+    logmsge("TODO: fabs()");
+    return x;
+}
+
+double frexp(double x, int *exp) {
+    logmsge("TODO: frexp()");
+    return x;
+}
+double modf(double x, double *ptr) {
+    logmsge("TODO: modf()");
+    return x;
+}
+
+double fmod(double x, double y) {
+    logmsge("TODO: fmod()");
+    return x;
+}
+double pow(double x, double y) {
+    logmsge("TODO; pow()");
+    return x;
+}
+
+double exp(double x) {
+    logmsge("TODO: exp()");
+    return x;
+}
+double ldexp(double x, int exp) {
+    logmsge("TODO: ldexp()");
+    return x;
+}
+
+double log(double x) {
+    logmsge("TODO: log()");
+    return x;
+}
+double log10(double x) {
+    logmsge("TODO: log10()");
+    return x;
+}
+
+double sqrt(double x) {
+    logmsge("TODO: sqrt()");
+    return x;
+}
+
+double atan(double x) {
+    logmsge("TODO: atan()");
+    return x;
+}
+double asin(double x) {
+    logmsge("TODO: asin()");
+    return x;
+}
+double acos(double x) {
+    logmsge("TODO: acos()");
+    return x;
+}
+
+double tan(double x) {
+    logmsge("TODO: tan()");
+    return x;
+}
+double sin(double x) {
+    logmsge("TODO: sin()");
+    return x;
+}
+double cos(double x) {
+    logmsge("TODO: cos()");
+    return x;
+}
+
+double atan2(double y, double x) {
+    logmsge("TODO: atan2()");
+    return x;
+}
+
+double tanh(double x) {
+    logmsge("TODO: tanh()");
+    return x;
+}
+double sinh(double x) {
+    logmsge("TODO: sinh()");
+    return x;
+}
+double cosh(double x) {
+    logmsge("TODO: cosh()");
+    return x;
+}
+
