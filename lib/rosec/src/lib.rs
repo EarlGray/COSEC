80edbd054a782baf37f97ae9af05a4c82f1deda3
Dmytro S <me@dmytrish.net>
2020-08-30 21:06:20 +0100

    lib/rosec updated to modern Cargo/Rust.

--- lib/rosec/src/lib.rs
+++ lib/rosec/src/lib.rs
@@ -2,13 +2,22 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
 extern {
     fn k_printf(msg: *const u8);
+
+    #[link_name="panic"]
+    fn k_panic(msg: *const u8) -> !;
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    let msg = match info.payload().downcast_ref::<&str>() {
+        Some(s) => s.as_ptr(),
+        _ => b"Rust panic\0".as_ptr(),
+    };
+    unsafe {
+        k_panic(msg);
+    }
 }
 
 #[no_mangle]
